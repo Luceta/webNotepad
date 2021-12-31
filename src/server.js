abbed7ac87a68b06a833c6efeddc4a6de6c33bef
Luceta <57941164+Luceta@users.noreply.github.com>
2022-01-01 04:04:11 +0900

    Add: style Notepage

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { process_params } from "express/lib/router";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
